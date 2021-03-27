b9f4f6c743a755c5afdf2a213484e38a44f8e3f6
ntsly <ngothisaoly@gmail.com>
2021-03-27 22:27:44 +0700

    Refactor code

--- src/main/java/congnghetuts/com/quarkus/http/HTTPResponse.java
+++ src/main/java/congnghetuts/com/quarkus/http/HTTPResponse.java
@@ -4,26 +4,22 @@ import javax.ws.rs.core.Response;
 
 public class HTTPResponse {
     public static Response success(String message, Object data) {
-        HTTPContentRespone httpContentRespone = new HTTPContentRespone();
-        httpContentRespone.code = 200;
-        httpContentRespone.message = message;
-        httpContentRespone.data = data;
-        return Response.ok(httpContentRespone).build();
+        return Response.ok(createContentResponse(200, message, data)).build();
     }
 
     public static Response badRequest(String message, Object data) {
-        HTTPContentRespone httpContentRespone = new HTTPContentRespone();
-        httpContentRespone.code = 400;
-        httpContentRespone.message = message;
-        httpContentRespone.data = data;
-        return Response.status(Response.Status.BAD_REQUEST).entity(httpContentRespone).build();
+        return Response.status(Response.Status.BAD_REQUEST).entity(createContentResponse(400, message, data)).build();
     }
 
     public static Response serverError(String message, Object data) {
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(createContentResponse(500, message, data)).build();
+    }
+
+    private static HTTPContentRespone createContentResponse(Integer code, String message, Object data) {
         HTTPContentRespone httpContentRespone = new HTTPContentRespone();
-        httpContentRespone.code = 500;
+        httpContentRespone.code = code;
         httpContentRespone.message = message;
         httpContentRespone.data = data;
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(httpContentRespone).build();
+        return httpContentRespone;
     }
 }
