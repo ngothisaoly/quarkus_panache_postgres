790bd543323fd010029e416253541ee383d80b23
ntsly <ngothisaoly@gmail.com>
2021-06-23 10:05:49 +0700

    Add  test case for findByNameAndEmail

--- src/test/java/congnghetuts/com/quarkus/panache/controller/PersonControllerTest.java
+++ src/test/java/congnghetuts/com/quarkus/panache/controller/PersonControllerTest.java
@@ -11,6 +11,8 @@ import javax.json.Json;
 import javax.json.JsonObject;
 
 import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -62,5 +64,48 @@ public class PersonControllerTest {
         given()
                 .when().get("/person")
                 .then().statusCode(200);
+    }
+
+    @Test
+    public void testGetByNameAndEmail() {
+        //Create person
+        PersonVO personVO = new PersonVO();
+        personVO.setName("name3");
+        personVO.setEmail("name3@gmail.com");
+        given()
+                .header("Content-Type","application/json")
+                .body(personVO)
+                .when().post("/person");
+
+        //Find by name and email
+        Response response =  given()
+                .header("Content-Type","application/json")
+                .pathParam("name", "name3")
+                .pathParam("email", "name3@gmail.com")
+                .when().get("/person/findByNameAndEmail/{name}/{email}");
+
+
+        response.then().statusCode(200);
+        JsonObject jsonObject =  Json.createReader(new StringReader(response.asString())).readObject();
+        Assertions.assertEquals("name3", jsonObject.getString("name"));
+        Assertions.assertEquals("name3@gmail.com", jsonObject.getString("email"));
+
+
+
+        Map<String, String> mapParams = new HashMap<>();
+        mapParams.put("name", "name3");
+        mapParams.put("email", "name3@gmail.com");
+
+         response =  given()
+                .header("Content-Type","application/json")
+                .pathParams(mapParams)
+                .when().get("/person/findByNameAndEmail/{name}/{email}");
+
+        response.then().statusCode(200);
+        jsonObject =  Json.createReader(new StringReader(response.asString())).readObject();
+        Assertions.assertEquals("name3", jsonObject.getString("name"));
+        Assertions.assertEquals("name3@gmail.com", jsonObject.getString("email"));
+
+
     }
 }
